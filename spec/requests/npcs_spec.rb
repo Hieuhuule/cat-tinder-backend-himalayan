83f3700e8dc76dd656cb433c3bd3707b9a4330d7
LEARN Academy <learnacademy@LEARNs-MacBook-Air.local>
2022-09-22 14:44:08 -0700

    added index request spec, added index endpoint, add create request spec, add a create endpoint, app not broken

--- spec/requests/npcs_spec.rb
+++ spec/requests/npcs_spec.rb
@@ -2,6 +2,37 @@ require 'rails_helper'
 
 RSpec.describe "Npcs", type: :request do
   describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+    it "gets a list of cats" do
+      Npc.create(
+        name: "Abigail",
+        address: "Pierre's General Store",
+        likes: "Amethyst",
+        image: "https://stardewvalleywiki.com/mediawiki/images/8/88/Abigail.png"
+      )
+      get '/npcs'
+
+      npc = JSON.parse(response.body)
+      expect(response).to have_http_status(200)
+      expect(npc.length).to eq 1
+    end
+  end
+  describe "POST /create" do
+    it "creates a npc" do
+      npc_params = {
+        npc: {
+          name: 'Linus',
+          address: 'Tent',
+          likes: 'Blueberry Tart',
+          image: 'https://stardewvalleywiki.com/mediawiki/images/3/31/Linus.png'
+        }
+      }
+      post '/npcs', params: npc_params
+      expect(response).to have_http_status(200)
+  
+      npc = Npc.first
+
+      expect(npc.name).to eq 'Linus'
+    end
   end
 end
+
