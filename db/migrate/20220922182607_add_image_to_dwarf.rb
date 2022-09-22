f0df7d638d388044f5fd182c1ae33c4fcb1493ac
LEARN Academy <learnacademy@LEARNs-MacBook-Air.local>
2022-09-22 11:44:46 -0700

    added rspec, added npc model, added seeds, added cors, app not broken

---
+++ db/migrate/20220922182607_add_image_to_dwarf.rb
@@ -0,0 +1,4 @@
+class AddImageToDwarf < ActiveRecord::Migration[7.0]
+  def change
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+npcs = [
+    {
+        name: "Abigail",
+        address: "Pierre's General Store",
+        likes: "Amethyst",
+        image: "https://stardewvalleywiki.com/mediawiki/images/8/88/Abigail.png"
+    },
+    {
+        
+        name: "Emily",
+        address: "2 Willow Lane",
+        likes: "Aquamarine",
+        image: "https://stardewvalleywiki.com/mediawiki/images/2/28/Emily.png"
+    },
+    {
+        
+        name: "Haley",
+        address: "2 Willow Lane",
+        likes: "Coconut",
+        image: "https://stardewvalleywiki.com/mediawiki/images/1/1b/Haley.png"
+    }
+]
+npcs.each do |each_npc|
+    Npc.create each_npc
+    puts "creating npc #{each_npc}"
+end
